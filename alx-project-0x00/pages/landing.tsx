24694df42a9b9470001b371267095a098dcf1c1a
loice okello <loicelaura@gmail.com>
2025-07-15 11:21:11 +0300

    updated landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,6 +10,7 @@ const Landing: React.FC =  () => {
         <Button title="Small Rounded-sm" styles="px-3 py-1 text-sm rounded-sm" />
         <Button title="Medium Rounded-md" styles="px-4 py-2 text-base rounded-md" />
         <Button title="Large Rounded-full" styles="px-6 py-3 text-lg rounded-full" />
+        <Button title="Extra Rounded-lg" styles="px-5 py-2 text-base rounded-lg" />
       </div>
     </div>
   );
