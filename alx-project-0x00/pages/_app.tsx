4bdcf8e029571889c08d13c70f268fa58e317fda
loice okello <loicelaura@gmail.com>
2025-07-15 09:11:11 +0300

    added Card.tsx and index.tx

--- alx-project-0x00/pages/_app.tsx
+++ alx-project-0x00/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
 }
+
+export default Home;
