215dade0e47dabec631d6549b9c93bd65923ea35
loice okello <loicelaura@gmail.com>
2025-07-15 10:32:20 +0300

    added landing.tsx and about.tsx

--- alx-project-0x00/pages/_app.tsx
+++ alx-project-0x00/pages/_app.tsx
@@ -1,10 +1,6 @@
-const Home: React.FC = () => {
-  return (
-    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-      <h1>Airbnb Application Clone system</h1>
-      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
-    </main>
-  )
-}
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default Home;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
